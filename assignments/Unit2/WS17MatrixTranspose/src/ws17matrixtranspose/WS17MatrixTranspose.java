c81494e731d1d0db369e9c4388c36a9507282885
FerBacuy <dia_nara15@hotmail.com>
2021-02-28 15:39:06 -0500

    Transpose

--- assignments/Unit2/WS17MatrixTranspose/src/ws17matrixtranspose/WS17MatrixTranspose.java
+++ assignments/Unit2/WS17MatrixTranspose/src/ws17matrixtranspose/WS17MatrixTranspose.java
@@ -16,145 +16,152 @@ public class WS17MatrixTranspose {
     /**
      * @param args the command line arguments
      */
-public static void main(String[] args) {
-                
-        float [][] a;
-        float [][] b;
-        float [][] t;
-        float [][] c;
-        int m=0;
-        int n=0;
-        
-        Scanner input = new Scanner (System.in);
-        System.out.println("Enter the matrix dimensions: ");
-        System.out.println("m: ");
-        m = input.nextInt();
-        System.out.println("n: ");
-        n = input.nextInt();
-        
-        a = new float [m][n];
-        b = new float [m][n];
-        c = new float [m][n];
-        t = new float [m][n];
-        
-        System.out.println("\nEnter the elements of the matrix: ");
-        
-        a = readMatrixElements(m, n, "A", input);
-        b = readMatrixElements(m, n, "A", input);
-        
-        for(int tab = 0; tab <= m / 2; tab++){
+    public static void main(String[] args) {
+
+        int[][] a;
+        int[][] b;
+        int[][] t;
+        int[][] c;
+        int m = 0;
+        int n = 0;
+
+        Scanner data = new Scanner(System.in);
+
+        System.out.println(" ENTER THE MATRIX DIMENSIONS ");
+        System.out.println("\n m (crows)->");
+        m = data.nextInt();
+
+        System.out.println("\n n (columns)->");
+        n = data.nextInt();
+
+        a = new int[m][n];
+        b = new int[m][n];
+        c = new int[m][n];
+
+        t = new int[n][m];
+
+        System.out.println("------ MATRIX ELEMENTS -------");
+
+        a = readMatrixElements(m, n, "A", data);
+        b = readMatrixElements(m, n, "A", data);
+
+        for (int tab = 0; tab <= n / 2; tab++) {
             System.out.println("\t");
         }
-        
-        System.out.println(" A ");
+        System.out.println(" A");
+
         prettyPrintMatrix(m, n, a);
         System.out.println("");
-        
-        System.out.println(" B ");
+
+        System.out.println(" B");
         prettyPrintMatrix(m, n, b);
-        System.out.println("");
-        
+
         transpose(n, m, t, a);
-        for(int tab = 0 ; tab <= m / 2; tab++){
+
+        for (int tab = 0; tab <= n / 2; tab++) {
             System.out.println("\t");
         }
-        System.out.println(" T ");
-        prettyPrintMatrix(n, m, t);
-        System.out.println("The sum of matrices is: ");
-        
+        System.out.println(" TRANSPOSE ");
+        prettyPrintMatrix(m, n, t);
+
+        System.out.println("-- THE ADDITION OF A + B --");
+
         c = addTwoMatrices(m, n, a, b);
         prettyPrintMatrix(m, n, c);
+    }
 
-}
-
-    public static float[][] readMatrixElements(int m, int n, String matrixName, Scanner input) {
-        float [][] matrix;
-        matrix=new float [m][n];
-        for(int i=0; i< m; i++){
-            System.out.println("--Row Numbers" + (i+1) + "--");
-            for(int j=0; j< n; j++){
-                 System.out.print(matrixName + "[" + (i+1) + "][" + (j+1)+ "] ->");
-                 matrix[i][j] = input.nextFloat();
+    public static int[][] readMatrixElements(int m, int n, String matrixName, Scanner input) {
+        int[][] matrix;
+        matrix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            System.out.println("-- ROWS " + (i + 1) + "--");
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrixName + "[" + (i + 1) + "][" + (j + 1) + "] ->");
+                matrix[i][j] = input.nextInt();
             }
         }
         return matrix;
     }
 
-    public static void transpose(int n, int m, float[][] t, float[][] a) {
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+    public static void transpose(int n, int m, int[][] t, int[][] a) {
+        //transposition operation (transpose)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 t[i][j] = a[j][i];
             }
         }
     }
 
-    public static float[][] addTwoMatrices(int m, int n, float[][] a, float[][] b) {
-        float[][] c;
-        c = new float [m][n];
-        
-        for(int i=0; i< m; i++){
-            for (int j=0; j< n; j++){
+    public static int[][] addTwoMatrices(int m, int n, int[][] a, int[][] b) {
+        int[][] c;
+        c = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 c[i][j] = a[i][j] + b[i][j];
             }
         }
         return c;
     }
 
-    public static void prettyPrintMatrix(int m, int n, float[][] matrix) {
-        for (int i=0; i< m; i++){
+    public static void prettyPrintMatrix(int m, int n, int[][] matrix) {
+        for (int i = 0; i < m; i++) {
             System.out.println("");
-            for(int j=0; j< n; j++){
-                if(j== 0){
-                    if(i== 0){
-                     System.out.println("\t" );
-                    }else if (i == m - 1){
-                        System.out.println("\t" );
-                    }else{
-                        System.out.println("\t" );
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    if (i == 0) {
+                        System.out.print("\t");
+                    } else if (i == m - 1) {
+                        System.out.print("\t");
+
+                    } else {
+                        System.out.print("\t");
                     }
                 }
-                System.out.println(matrix[i][j]);
-                if (j > -1 && j < (n - 1)){
-                    System.out.println("\t" );
+                System.out.print(matrix[i][j]);
+                if (j > -1 && j < (n - 1)) {
+                    System.out.print("\t");
                 }
-                if ( j== (n - 1)){
-                    if (i == 0){
-                        System.out.println("\t" );
-                    } else if ( i == m - 1){
-                        System.out.println("\t" );
-                    }else{
-                        System.out.println("\t" );
+                if (j == (n - 1)) {
+                    if (i == 0) {
+                        System.out.print("\t");
+                    } else if (i == m - 1) {
+                        System.out.print("\t");
+
+                    } else {
+                        System.out.print("\t");
                     }
                 }
             }
         }
         System.out.println("");
     }
-    public static void printMatrix(int m, int n, float[][] matrix){
-        for (int i=0; i< m; i++){
+
+    public static void printMatrix(int m, int n, int[][] matrix) {
+        for (int i = 0; i < m; i++) {
             System.out.println("");
-            for(int j=0; j< n; j++){
-                if(j== 0){
-                    if(i== 0){
-                     System.out.println("\t");
-                    }else if (i == m - 1){
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    if (i == 0) {
                         System.out.println("\t");
-                        
-                    }else{
+                    } else if (i == m - 1) {
+                        System.out.println("\t");
+
+                    } else {
                         System.out.println("\t");
                     }
                 }
                 System.out.println(matrix[i][j]);
-                if (j > -1 && j < (n - 1)){
+                if (j > -1 && j < (n - 1)) {
                     System.out.println("\t");
                 }
-                if ( j== (n - 1)){
-                    if (i == 0){
+                if (j == (n - 1)) {
+                    if (i == 0) {
                         System.out.println("\t");
-                    } else if ( i == m - 1){
+                    } else if (i == m - 1) {
                         System.out.println("\t");
-                        
-                    }else{
+
+                    } else {
                         System.out.println("\t");
                     }
                 }
@@ -162,5 +169,4 @@ public static void main(String[] args) {
         }
         System.out.println("");
     }
- 
 }
